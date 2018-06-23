259bcfe3d11b5179aa4775a338f8fb1268e34763
xi <2211867779@qq.com>
2018-06-23 18:50:00 +0800

    rem适配代码

---
+++ index.js
@@ -0,0 +1,13 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if(clientWidth>960) clientWidth=960;//这里限制最大的宽度尺寸，从而实现PC端的两边留白等
+            docEl.style.fontSize = 10 * (clientWidth / 320) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
